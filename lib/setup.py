3dbb53ecdf4d32f326b40fbdbe144d38b5285a2a
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2020-06-08 17:26:48 +0700

    rm

--- lib/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name='speedup',
-    ext_modules=cythonize("lib.speedup.pyx", language_level = "3"),
-    zip_safe=False,
-)
