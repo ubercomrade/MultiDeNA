63f4fdddf9fc9270f5b3fe29c69534fa0b7b0fd8
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2020-06-10 12:54:04 +0700

    fix import

--- pipeline.py
+++ pipeline.py
@@ -24,6 +24,7 @@ from tools.summary import write_peaks_classification
 from tools.scan_best_by_pwm import scan_best_by_pwm
 from tools.scan_best_by_bamm import scan_best_by_bamm
 from tools.scan_best_by_inmode import scan_best_by_inmode
+from tools.extract_sites import extract_sites
 from lib.common import check_threshold_table, check_bootstrap
 
 def prepare_data(path_to_genome, bed_path, bed, fasta, train_sample_size, test_sample_size):
