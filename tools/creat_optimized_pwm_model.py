fcd74538527372e2df671061e937642cc8510969
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2020-09-15 14:10:44 +0700

    fix

--- tools/creat_optimized_pwm_model.py
+++ tools/creat_optimized_pwm_model.py
@@ -151,7 +151,7 @@ def de_novo_with_oprimization_pwm(peaks_path, path_to_java, path_to_chipmunk,
     counter = 6000000
     if not os.path.exists(tmp_dir):
         os.mkdir(tmp_dir)
-    sites, length, fpr_current = learn_optimized_pwm(peaks_path, counter, path_to_java, 
+    sites, length = learn_optimized_pwm(peaks_path, counter, path_to_java, 
         path_to_chipmunk, tmp_dir, cpu_count, tpr, pfpr)
     shutil.rmtree(tmp_dir)
     pcm = make_pcm(sites)
