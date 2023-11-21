b9b993b4ddd46394cf2481c274741685f10262e2
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2023-11-21 21:46:34 +0700

    example

---
+++ example/multidena_example_run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+MultiDeNa.py ./PEAKS042778_PIF5_chr_1.bed \
+tair10 \
+./Arabidopsis_thaliana.TAIR10.dna.chromosome.1.fa \
+./PEAKS042778_PIF5_results \
+pwm-streme bamm \
+-t 500 -T 1000
